fea9e39423b03737f5dcc85f0cf5768ab05ee0f8
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-17 13:11:02 -0400

    ISSUE-4: adding initial project structure, including database entities and liquibase config

---
+++ src/main/java/com/tncalculator/tncalculatorapi/TNCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.tncalculator.tncalculatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TNCalculatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TNCalculatorApiApplication.class, args);
+    }
+
+}
