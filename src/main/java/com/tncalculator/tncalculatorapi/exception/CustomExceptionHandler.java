7f7933f61ec5705fd695c6b96362affad1632e41
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-19 14:57:59 -0400

    ISSUE-13: adding AOP to track balance changes
    Added a bunch of changes here, all in order to track balance changes,  I  used AOP and two annotations for that. Also added some custom exceptions for InsufficientBalance and UserNotFound
    
    PD: This was done with previous commit work, but decided to split them for a better understanding.

---
+++ src/main/java/com/tncalculator/tncalculatorapi/exception/CustomExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.tncalculator.tncalculatorapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<ErrorResponse> handleInsufficientBalanceException(InsufficientBalanceException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+}
