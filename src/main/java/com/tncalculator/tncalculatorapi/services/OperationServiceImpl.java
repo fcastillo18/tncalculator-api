ff6703e5717322da311cbf25fa10fca392606d1b
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-19 14:40:27 -0400

    ISSUE-13: Implement user balance tracking for each request
    Also, modified corresponding unit test after new implementation changes

--- src/main/java/com/tncalculator/tncalculatorapi/services/OperationServiceImpl.java
+++ src/main/java/com/tncalculator/tncalculatorapi/services/OperationServiceImpl.java
@@ -3,6 +3,7 @@ package com.tncalculator.tncalculatorapi.services;
 import com.tncalculator.tncalculatorapi.model.Operation;
 import com.tncalculator.tncalculatorapi.model.OperationRequest;
 import com.tncalculator.tncalculatorapi.model.Record;
+import com.tncalculator.tncalculatorapi.model.User;
 import com.tncalculator.tncalculatorapi.repository.OperationRepository;
 import com.tncalculator.tncalculatorapi.repository.RecordRepository;
 import com.tncalculator.tncalculatorapi.repository.UserRepository;
@@ -29,16 +30,31 @@ public class OperationServiceImpl implements OperationService {
 
     public Record subtract(OperationRequest request) {
         Operation operation = operationRepository.findByType(Operation.OperationType.SUBTRACTION);
+        User user = userRepository.findById(request.getUserId()).orElseThrow();
+
+        if(user.getBalance().compareTo(BigDecimal.ZERO) <= 0){
+            throw new RuntimeException("User has no balance to perform this operation");
+            // TODO: return a response to the user that the operation was not successful
+        }
+        // Update the user balance
+        user.setBalance(user.getBalance().subtract(operation.getCost())); // cost of the SUBTRACTION operation
+        // TODO What if I add Spring AOP to validate if the user got balance before executing the operation,
+        //  but also for updating the balance when the endpoint successfully executed?
+
         Record record = Record.builder()
                 .id(1L)
                 .user(userRepository.findById(request.getUserId()).orElseThrow())
                 .operation(operation)
                 .amount(BigDecimal.valueOf(request.getNum1() - request.getNum2()))
-                .userBalance(BigDecimal.valueOf(10))
+                .userBalance(user.getBalance()) // FIXME I dont think I need the user balance on this table, circle back on this later
                 .operationResponse("Subtract") // TODO might need an improvement here
                 .date(LocalDateTime.now())
                 .build();
         recordRepository.save(record);
+
+        // Update the user entity, to persist balance changes
+        userRepository.save(user);
+
         return record;
     }
 
