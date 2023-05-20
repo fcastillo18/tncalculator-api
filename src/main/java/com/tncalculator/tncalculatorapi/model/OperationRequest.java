cc5353b3a752c6e0ebec67b4564a807563ebc0b7
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-20 18:21:51 -0400

    ISSUE-09: Design squareRoot endpoint
    Also, updated unit test to cover new operation

--- src/main/java/com/tncalculator/tncalculatorapi/model/OperationRequest.java
+++ src/main/java/com/tncalculator/tncalculatorapi/model/OperationRequest.java
@@ -12,4 +12,9 @@ public class OperationRequest {
     private Long userId;
     private double num1;
     private double num2;
+
+    public OperationRequest(Long userId, double num1) {
+        this.userId = userId;
+        this.num1 = num1;
+    }
 }
