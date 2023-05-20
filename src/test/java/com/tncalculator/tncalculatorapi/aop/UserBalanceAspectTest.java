634bc561b2f4bff12785db189171302686b34eda
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-20 10:58:55 -0400

    ISSUE-07: removing unnecessary operationType property in OperationRequest.
    Doing a little bit of refactor by adding that property into the ValidateUserBalance interface, this way we can set the operationType with a different value when calling the annotation, and that avoid to add another parameter to the body in the service call.

--- src/test/java/com/tncalculator/tncalculatorapi/aop/UserBalanceAspectTest.java
+++ src/test/java/com/tncalculator/tncalculatorapi/aop/UserBalanceAspectTest.java
@@ -1,5 +1,6 @@
 package com.tncalculator.tncalculatorapi.aop;
 
+import com.tncalculator.tncalculatorapi.aop.annotation.ValidateUserBalance;
 import com.tncalculator.tncalculatorapi.exception.InsufficientBalanceException;
 import com.tncalculator.tncalculatorapi.model.Operation;
 import com.tncalculator.tncalculatorapi.model.OperationRequest;
@@ -14,6 +15,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.annotation.Annotation;
 import java.math.BigDecimal;
 import java.util.Optional;
 
@@ -33,30 +35,52 @@ public class UserBalanceAspectTest {
     private UserBalanceAspect userBalanceAspect;
 
     private final Operation.OperationType ADDITION_TYPE = Operation.OperationType.ADDITION;
+
+    // Custom method to create a ValidateUserBalance annotation with the given operation type
+    private ValidateUserBalance createValidateUserBalance(Operation.OperationType operationType) {
+        return new ValidateUserBalance() {
+            @Override
+            public Operation.OperationType operation() {
+                return operationType;
+            }
+
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return ValidateUserBalance.class;
+            }
+        };
+    }
+
     @Test
     public void testValidateUserBalance_WithSufficientBalance() {
         // Prepare
-        OperationRequest request = new OperationRequest(1L, 10, 5, ADDITION_TYPE) ;
+        Operation.OperationType operationType = Operation.OperationType.ADDITION;
+        OperationRequest request = new OperationRequest(1L, 10, 5);
 
         User user = new User();
         user.setBalance(BigDecimal.valueOf(15));
-        Operation operation = new Operation(1L, ADDITION_TYPE,BigDecimal.valueOf(10));
+        Operation operation = new Operation(1L, operationType, BigDecimal.valueOf(10));
         operation.setCost(BigDecimal.valueOf(10));
 
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-        when(operationRepository.findByType(ADDITION_TYPE)).thenReturn(operation);
+        when(operationRepository.findByType(operationType)).thenReturn(operation);
 
-        // Execute
-        userBalanceAspect.validateUserBalance(request);
+        // validateUserBalance method is annotated with ValidateUserBalance annotation,  so we can use it here to test the aspect
+        ValidateUserBalance validateUserBalance = createValidateUserBalance(operationType);
 
-        // Verify - No exception should be thrown
-        Assertions.assertDoesNotThrow(() -> userBalanceAspect.validateUserBalance(request), "No exception should be thrown");
+        // Execute and Verify - No exception should be thrown
+        Assertions.assertDoesNotThrow(() ->
+                userBalanceAspect.validateUserBalance(request, validateUserBalance),
+                "No exception should be thrown");
     }
+
+
 
     @Test
     public void testValidateUserBalance_WithInsufficientBalance() {
         // Prepare
-        OperationRequest request = new OperationRequest(1L, 10, 5, Operation.OperationType.ADDITION) ;
+        Operation.OperationType operationType = Operation.OperationType.ADDITION;
+        OperationRequest request = new OperationRequest(1L, 10, 5) ;
         User user = new User();
         user.setBalance(BigDecimal.valueOf(0)); // zero balance
         Operation operation = new Operation(1L, ADDITION_TYPE,BigDecimal.valueOf(10));
@@ -65,8 +89,11 @@ public class UserBalanceAspectTest {
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
         when(operationRepository.findByType(ADDITION_TYPE)).thenReturn(operation);
 
+        // validateUserBalance method is annotated with ValidateUserBalance annotation,  so we can use it here to test the aspect
+        ValidateUserBalance validateUserBalance = createValidateUserBalance(operationType);
+
         // Execute and Verify
-        Assertions.assertThrows(InsufficientBalanceException.class, () -> userBalanceAspect.validateUserBalance(request));
+        Assertions.assertThrows(InsufficientBalanceException.class, () -> userBalanceAspect.validateUserBalance(request, validateUserBalance));
     }
 
     @Test
