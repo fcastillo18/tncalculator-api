95107dd311a333d8adfa1f8b8de22b414d29258d
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-21 13:42:15 -0400

    ISSUE-20: Design methods endpoints for /record
    Create a new /record/all GET method that will retrieve all the records from the database
    Create a new /record/create POST to create a record
    Create a new /record/delete DELETE method to delete a record, Records should be soft-deleted only.

--- src/main/java/com/tncalculator/tncalculatorapi/repository/RecordRepository.java
+++ src/main/java/com/tncalculator/tncalculatorapi/repository/RecordRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
-    // additional custom queries if needed
+
 }
