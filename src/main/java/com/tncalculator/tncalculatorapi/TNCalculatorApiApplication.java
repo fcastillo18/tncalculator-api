e6296a116068cc57ca9c5826fd9bd0489a8192ba
Franklin Castillo <franklincastillo18@gmail.com>
2023-05-22 17:10:17 -0400

    ISSUE-11: Containerize the application using Docker
    -Create Dockerfile for `dev` and `staging` environments.
    -Added multiple profiles to the application
    -Update README.md

--- src/main/java/com/tncalculator/tncalculatorapi/TNCalculatorApiApplication.java
+++ src/main/java/com/tncalculator/tncalculatorapi/TNCalculatorApiApplication.java
@@ -1,14 +1,24 @@
 package com.tncalculator.tncalculatorapi;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+@Log4j2
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class TNCalculatorApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TNCalculatorApiApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> log.info("Message from application.properties --> " + environment.getProperty("message-from-application-properties"));
     }
 
 }
